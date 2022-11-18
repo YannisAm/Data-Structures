019b6dd1dd95be7f4091ed62d09ed5a609afcab5
Yannis <giannamanat@gmail.com>
2022-11-18 20:49:33 +0200

    Finished stack

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
@@ -6,41 +7,44 @@ namespace Stack
 {
     public class Stack : StackBase
     {
-        private int top = -1;
-        private int[]? stack;
-        private int size;
+        private int top = -1;       //here we initialize the top variable assigning it the value of -1
+        private int[]? stack;       //we initialize the stack array, but we will assign it's size in the constructor to make it more dynamic
+        private int size;           //here we initialize the size of the array but it's value we will be assigned with the constructor
 
-        public Stack(int size)
-        {
+        public Stack(int size)      //we initialize the size of our stack declaring it in the constuctor and checking if it is grater than zero because
+        {                           //there is no stack with zero or negative size
             if (size > 0)
-                this.stack = new int[size];
+            {
+                this.size = size;
+                stack = new int[size];
+            }
             else
                 Console.WriteLine("You can not have a stask, with zero or negative size");
         }
 
-        public int GetTop()
-        {
+        public int GetTop()                     //we initialize the getter for the variable top. if the top is in the limits of the size of the stack
+        {                                       //then we return it else we send an exception
             try
             {
-                if (size > 0)
+                if (top > -1 && top <= size-1)
                     return top;
                 else
                     throw new Exception();
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Stack overflow: {ex.Message}");
+                Console.WriteLine($"You can not take the top value: {ex.Message}");
                 return -1;
             }
             
             
         }
 
-        public int GetSize()
+        public int GetSize()                
         {
-            try
-            {
-                if (size >= 0)
+            try                             //we initialize the getter for the size of the stack. if the size is greater than zero, that means that we
+            {                               //have initialized it and we can return it, else an exception is thrown.
+                    if (size >= 0)
                     return size;
                 else
                     throw new Exception();
@@ -52,33 +56,24 @@ namespace Stack
             }
         }
 
-        private bool IsFunctional()
+        private bool IsFunctional()                 //it's checked if the stack is within the limits
         {
-            if (top > -1 && top <= size - 1)
-                return true;
-            else
-                return false;
+            return (top >= -1 && top <= size-1);
         }
 
-        private bool IsFull()
+        private bool IsFull()                       //it's checks if the stack is full or not
         {
-            if (top > size - 1)
-                return true;
-            else
-                return false;
+            return top == size - 1;
         }
 
-        private bool IsEmpty()
+        private bool IsEmpty()                      //it's checks if the stack is empty or not
         {
-            if (top < size-1)
-                return true;
-            else
-                return false;
+            return top == -1;
         }
 
-        public void Push(int number)
-        {
-            if(IsEmpty() && IsFunctional())
+        public void Push(int number)                //when the method Push is called then we put an element to the last position of our stack. To do so 
+        {                                           //first we check if the stack is not full and is functional
+            if (!IsFull() && IsFunctional())
             {
                 stack[++top] = number;
                 Console.WriteLine($"The {number} has been pushed");
@@ -88,9 +83,41 @@ namespace Stack
 
         }
 
+        public void Pop()                       //when the method Pop is called then the last element of the stack is beeing removed. Before doing this
+        {                                       //we check if the stack is empty or not
+            if (!IsEmpty())
+            {
+                int outNumber = stack[top];
+                stack[top--] = 0;
+                Console.WriteLine($"The {outNumber} has been poped out of the stack!");
+            }
+            else
+                Console.WriteLine("You can not pop an element out of an empty stack.");
 
-        public void StackPrint()
+        }
+
+
+        public void StackPrint()                    //calling this method we print the elements in the stack
         {
+            try
+            {
+                if (top >= 0)
+                {
+                    Console.WriteLine("-------------- size = " + (top + 1) + " ----------");
+
+                    for (int i = 0; i <= top; i++)
+                        Console.Write(stack[i] + " ");
+                    Console.WriteLine();
+                    Console.WriteLine("----------------------------------");
+                }
+                else
+                {
+                    throw new Exception();        
+                }                                                                                              }
+            catch (Exception)
+            {
+                Console.WriteLine("You can not print an empty stack");
+            }
         }
     }
 
--- Stack/StackBase.cs
+++ Stack/StackBase.cs
@@ -5,8 +5,22 @@
         public static void Main(string[] args)
 
         {
-            Stack stack = new Stack(2);
+            Stack stack = new Stack(4);
             stack.StackPrint();
+            stack.Push(5);
+            stack.Push(3);
+            stack.Push(7);
+            stack.Push(90);
+            stack.Push(23);
+            stack.Pop();
+            stack.Pop();
+            stack.Pop();
+            stack.StackPrint();
+            stack.Push(8);
+            stack.Push(9);
+            stack.Push(10);
+            stack.StackPrint();
+            stack.GetTop();
         }
     }
 }
